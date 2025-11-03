1e8f12bfe7c989a64da25d9237766d3fc79c11b6
DadBytes <bpmckim@proton.me>
2025-11-03 12:16:48 -0500

    Add lsp through mason.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -41,7 +41,7 @@ vim.opt.confirm = true
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    { import = "plugins.lsp" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
